e1af306962caa6f9da7c74afef34276ad38b9d43
Davud Samed Tombul <55891311+ProgrammerAnchowy@users.noreply.github.com>
2021-02-02 20:12:36 +0300

    PasswordRecovery, PasswordVerification ve ResetPassword ekranları oluşturuldu.

--- src/navigation.js
+++ src/navigation.js
@@ -6,6 +6,9 @@ import WelcomeScreen from './views/Welcome.js';
 import LoginScreen from './views/Login';
 import RegisterScreen from './views/Register';
 import VerificationScreen from './views/VerificationCode';
+import ResetPasswordScreen from './views/ResetPassword';
+import PasswordVerificationScreen from './views/PasswordVerification';
+import PasswordRecoveryScreen from './views/PasswordRecovery';
 
 const LoginStack = createStackNavigator();
 
@@ -19,9 +22,22 @@ function Navigation() {
       <LoginStack.Screen name={'Login'} component={LoginScreen} />
       <LoginStack.Screen name={'Register'} component={RegisterScreen} />
       <LoginStack.Screen
+        name={'ResetPassword'}
+        component={ResetPasswordScreen}
+      />
+      <LoginStack.Screen
+        name={'PasswordVerification'}
+        component={PasswordVerificationScreen}
+      />
+
+      <LoginStack.Screen
         name={'VerificationCode'}
         component={VerificationScreen}
       />
+        <LoginStack.Screen
+            name={'PasswordRecovery'}
+            component={PasswordRecoveryScreen}
+        />
     </LoginStack.Navigator>
   );
 }
